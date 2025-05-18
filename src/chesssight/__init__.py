812094b70ad0f1e06299f0fbc6093d6b8835011b
Thibaut CHAUFFIER <tchauffi@gmail.com>
2025-05-18 18:32:38 +0200

    ADD: add initial module documentation and metadata to __init__.py

---
+++ src/chesssight/__init__.py
@@ -0,0 +1,14 @@
+"""ChessSight - A Computer Vision Chess Analysis Tool.
+
+This package provides tools for analyzing chess positions using computer vision.
+
+Author: Thibaut CHAUFFIER
+Email: tchauffi@gmail.com
+Version: 0.1.0
+License: MIT
+"""
+
+__version__ = "0.1.0"
+__author__ = "Thibaut CHAUFFIER"
+__email__ = "tchauffi@gmail.com"
+__license__ = "MIT"
